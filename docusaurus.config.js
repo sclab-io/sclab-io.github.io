2777c52738158ad23c3e231b437a634e0010e8f2
SungYong Jang <jsy@sclab.io>
2023-03-15 19:14:03 +0900

    add deploy script

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -19,6 +19,7 @@ const config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'sclab-io', // Usually your GitHub org/user name.
   projectName: 'sclab-io.github.io', // Usually your repo name.
+  trailingSlash: false,
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
@@ -126,6 +127,10 @@ const config = {
               {
                 label: 'Blog',
                 to: '/blog',
+              },
+              {
+                label: 'SCLAB Studio',
+                href: 'https://www.sclab.io',
               },
               {
                 label: 'GitHub',
